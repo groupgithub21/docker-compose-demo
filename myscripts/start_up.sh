4a507d0ec1db1a0ac3535b9c76495f780afd4cb2
Your Name <you@example.com>
2021-12-03 03:36:56 +0000

    b4 server delete

--- myscripts/start_up.sh
+++ myscripts/start_up.sh
@@ -1,24 +1,7 @@
 # Important, Do this first. https://senzing.zendesk.com/hc/en-us/articles/115002408867-Quickstart-Guide
-export GIT_ACCOUNT=senzing
-export GIT_REPOSITORY=docker-compose-demo
-export GIT_ACCOUNT_DIR=~/${GIT_ACCOUNT}.git
-export GIT_REPOSITORY_DIR="${GIT_ACCOUNT_DIR}/${GIT_REPOSITORY}"
-
-export SENZING_VOLUME=/opt/senzing
-
-export SENZING_DATA_DIR=${SENZING_VOLUME}/data
-export SENZING_DATA_VERSION_DIR=${SENZING_DATA_DIR}/2.0.0
-export SENZING_ETC_DIR=${SENZING_VOLUME}/etc
-export SENZING_G2_DIR=${SENZING_VOLUME}/g2
-export SENZING_VAR_DIR=${SENZING_VOLUME}/var
-
-export POSTGRES_DIR=${SENZING_VAR_DIR}/postgres
-export RABBITMQ_DIR=${SENZING_VAR_DIR}/rabbitmq
-
 sudo mkdir -p ${RABBITMQ_DIR}
 sudo chmod 770 ${RABBITMQ_DIR}
 
-export SENZING_ACCEPT_EULA="I_ACCEPT_THE_SENZING_EULA"
 
 cd ${GIT_REPOSITORY_DIR}
 
@@ -38,17 +21,17 @@ sudo \
 #Data Ingestion
 
 curl -X 'POST' \
-  'http://192.168.180.237:8250/data-sources?dataSource=CIVIL&dataSource=WATCHLIST&withRaw=false' \
+  'http://192.168.180.237:8250/data-sources?dataSource=HR&dataSource=ACRA&withRaw=false' \
   -H 'accept: application/json; charset=UTF-8' \
   -H 'Content-Type: application/json; charset=UTF-8'
 
-curl --location --request POST 'http://192.168.180.237:8250/bulk-data/load?dataSource=CIVIL&progressPeriod=3000' \
+curl --location --request POST 'http://192.168.180.237:8250/bulk-data/load?dataSource=HR&progressPeriod=3000' \
 --header 'Content-Type: application/json' \
---data-binary '@/home/hanifa/senzing/mydata_1/cpib_demo/civil_servant.json'
+--data-binary '@/home/senzing-hanifa/senzing.git/docker-compose-demo/myscripts/data/HRDataset_v14.json'
 
-curl --location --request POST 'http://192.168.180.237:8250/bulk-data/load?dataSource=WATCHLIST&progressPeriod=3000' \
+curl --location --request POST 'http://192.168.180.237:8250/bulk-data/load?dataSource=ACRA&progressPeriod=3000' \
 --header 'Content-Type: application/json' \
---data-binary '@/home/hanifa/senzing/mydata_1/cpib_demo/director_watchlist.json'
+--data-binary '@/home/senzing-hanifa/senzing.git/docker-compose-demo/myscripts/data/director_watchlist.json'
 
 
 
